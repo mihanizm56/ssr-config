ada88bfe2eb764fc680452ceb7fcb80928748e52
mihanizm56 <m727507.56@mail.ru>
2020-12-21 02:18:17 +0300

    test update

--- bin/ssr-scripts.js
+++ bin/ssr-scripts.js
@@ -10,33 +10,34 @@ const path = require('path');
 const spawn = require('react-dev-utils/crossSpawn');
 const { getConsoleArgs } = require('../utils/get-args');
 
-const { command } = getConsoleArgs(process.argv);
-
 const allowedCommands = ['build','start'];
+const command = process.argv[2]
 
-if (allowedCommands.includes(command)) {
-  const result = spawn.sync(
-    `npm run ${command}`,
-    { stdio: 'inherit', shell: true }
-  );
-
-  if (result.signal) {
-    if (result.signal === 'SIGKILL') {
-      console.log(
-        'The build failed because the process exited too early. ' +
-          'This probably means the system ran out of memory or someone called ' +
-          '`kill -9` on the process.'
-      );
-    } else if (result.signal === 'SIGTERM') {
-      console.log(
-        'The build failed because the process exited too early. ' +
-          'Someone might have called `kill` or `killall`, or the system could ' +
-          'be shutting down.'
-      );
-    }
-    process.exit(1);
-  }
-  process.exit(result.status);
-} else {
-  console.log('Unknown script "' + script + '".');
+if(!command || !allowedCommands.includes(command)){
+  console.log(`Unknown command ${command}`);
+  return
 }
+
+const result = spawn.sync(
+  `npm run ${command}`,
+  { stdio: 'inherit', shell: true }
+);
+
+if (result.signal) {
+  if (result.signal === 'SIGKILL') {
+    console.log(
+      'The build failed because the process exited too early. ' +
+      'This probably means the system ran out of memory or someone called ' +
+      '"kill -9" on the process.'
+    );
+  } else if (result.signal === 'SIGTERM') {
+    console.log(
+      'The build failed because the process exited too early. ' +
+      'Someone might have called `kill` or `killall`, or the system could ' +
+      'be shutting down.'
+    );
+  }
+  process.exit(1);
+}
+
+process.exit(result.status);
