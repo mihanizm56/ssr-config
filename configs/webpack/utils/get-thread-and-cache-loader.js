835a51fb4c4db8194367fe3317262b277bb68b6e
mihanizm56 <m727507.56@mail.ru>
2021-02-07 18:09:51 +0300

    build(configure-package): add multi-thread choise

--- configs/webpack/utils/get-thread-and-cache-loader.js
+++ configs/webpack/utils/get-thread-and-cache-loader.js
@@ -1,21 +1,11 @@
 import os from 'os';
 
+const singleThreaded = process.env.SINGLE_THREADED === 'true';
+
 export const getCacheAndThreadLoaderConfig = (isProduction) =>
   isProduction
     ? [
-        // {
-        //   loader: 'thread-loader',
-        //   options: {
-        //     workers: os.cpus().length - 1,
-        //     poolRespawn: false,
-        //     workerParallelJobs: 50,
-        //     poolParallelJobs: 200,
-        //   },
-        // },
-      ]
-    : [
-        { loader: 'cache-loader' },
-        {
+        !singleThreaded && {
           loader: 'thread-loader',
           options: {
             workers: os.cpus().length - 1,
@@ -24,4 +14,16 @@ export const getCacheAndThreadLoaderConfig = (isProduction) =>
             poolParallelJobs: 200,
           },
         },
-      ];
+      ].filter(Boolean)
+    : [
+        { loader: 'cache-loader' },
+        !singleThreaded && {
+          loader: 'thread-loader',
+          options: {
+            workers: os.cpus().length - 1,
+            poolRespawn: false,
+            workerParallelJobs: 50,
+            poolParallelJobs: 200,
+          },
+        },
+      ].filter(Boolean);
