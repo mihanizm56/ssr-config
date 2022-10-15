66a452822fa24e199ea3fc4a1d81a8f22ff488d5
mihanizm56 <m727507.56@mail.ru>
2022-10-16 00:10:39 +0300

    build(configure-package): update

--- configs/babel-node.config.js
+++ configs/babel-node.config.js
@@ -14,5 +14,7 @@ module.exports = {
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-transform-exponentiation-operator',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
   ],
 };
