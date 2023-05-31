f7cac21c0bbd1caed59cbdc4768381fd9801ba6d
mihanizm <mihanizm@wisebits.com>
2023-05-31 03:21:06 +0300

    build(configure-package): update

--- scripts/utils/enrich-server-config.js
+++ scripts/utils/enrich-server-config.js
@@ -4,9 +4,9 @@ import { HotModuleReplacementPlugin } from 'webpack';
 
 export const enrichServerConfig = serverConfig => {
   serverConfig.output.hotUpdateMainFilename =
-    'updates/[contenthash].hot-update.json';
+    'updates/[fullhash].hot-update.json';
   serverConfig.output.hotUpdateChunkFilename =
-    'updates/[id].[contenthash].hot-update.js';
+    'updates/[id].[fullhash].hot-update.js';
 
   serverConfig.plugins.push(new HotModuleReplacementPlugin());
 };
