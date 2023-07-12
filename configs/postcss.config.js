10a350d59f64204eb0679209ef5345bba5f2388f
mihanizm <mihanizm@wisebits.com>
2023-07-12 13:30:16 +0300

    build(configure-package): update

--- configs/postcss.config.js
+++ configs/postcss.config.js
@@ -10,12 +10,6 @@ module.exports = () => {
     // The list of plugins for PostCSS
     // https://github.com/postcss/postcss
     plugins: [
-      // W3C calc() function, e.g. div { height: calc(100px - 2em); }
-      // https://github.com/postcss/postcss-calc
-      require('postcss-calc')(),
-      // Postcss flexbox bug fixer
-      // https://github.com/luisrudge/postcss-flexbugs-fixes
-      require('postcss-flexbugs-fixes')(),
       // Add vendor prefixes to CSS rules using values from caniuse.com
       // https://github.com/postcss/autoprefixer
       require('autoprefixer')({
