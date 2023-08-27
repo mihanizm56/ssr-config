3100a533b9b7572e42d56a702b4a34ba374d96e7
mihanizm56 <m727507.56@mail.ru>
2023-08-28 00:15:20 +0300

    build(configure-package): update

--- configs/babel-node.config.js
+++ configs/babel-node.config.js
@@ -12,7 +12,6 @@ module.exports = {
   plugins: [
     '@babel/plugin-transform-runtime',
     '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-nullish-coalescing-operator',
   ],
